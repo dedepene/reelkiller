33d63dc3b3aac910d9260dec46d0fa6819d76b88
dedepene <rvelitchkov@gmail.com>
2022-08-10 14:41:50 +0300

    cleanup on isle 4

--- facebook.js
+++ facebook.js
@@ -1,19 +1,25 @@
+//this function instead of e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode
+
+function getParentNode(element, level =1) {
+    while (level-- > 0) {
+        element = element.parentNode;
+        if (!element) return null; 
+    }
+    return element;
+}
+
 function removeReels() {
     reels = document.getElementsByClassName('d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d3f4x2em mdeji52x a5q79mjw g1cxx5fr lrazzd5p oo9gr5id');
-    console.log('type of reels: ', typeof(reels), "content of reels: ", reels);
     for (let i=0; i <reels.length; i++){
         if (reels[i].innerHTML.indexOf('Reels and short') != -1) {
-            // console.log('grandparent was:', (reels[i].parentNode).parentNode.parentNode.parentNode.parentNode.parentNode);
-            properDiv = (reels[i].parentNode).parentNode.parentNode.parentNode.parentNode.parentNode;
-            //reels[i].parentNode.removeChild(reels[i]);
-            properDiv.remove();
+            properNode = getParentNode (reels[i], 6);
+            properNode.remove();
             console.log('reels was found and terminated');
 
             chrome.storage.sync.get("kills", ({ kills }) => {
                 kills++
                 chrome.storage.sync.set({ kills });
 
-                //changeColor.style.backgroundColor = color;
               });
 
         }
@@ -23,8 +29,6 @@ function removeReels() {
 
     }
 }
-
-//removeReels();
 
 document.addEventListener ('scroll', removeReels);
 
--- popup.js
+++ popup.js
@@ -5,22 +5,3 @@ chrome.storage.sync.get("kills", ({ kills }) => {
   const div = document.getElementById('counter');
   div.textContent = `Reels killed: ${kills}`;
 });
-
-  // function setPageBackgroundColor() {
-  //   reels = document.getElementsByClassName('d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d3f4x2em mdeji52x a5q79mjw g1cxx5fr lrazzd5p oo9gr5id');
-  //   console.log(reels.length);
-  //   for (let i=0; i <reels.length; i++){
-  //       if (reels[i].innerHTML.indexOf('Reels and short') != -1) {
-  //           console.log('grandparent was:', (reels[i].parentNode).parentNode.parentNode.parentNode.parentNode.parentNode);
-  //           properDiv = (reels[i].parentNode).parentNode.parentNode.parentNode.parentNode.parentNode;
-  //           console.log('reels was found and terminated');
-  //           reels[i].parentNode.removeChild(reels[i]);
-  //           properDiv.remove();
-  //       }
-  //       else {
-  //           console.log('got no reels');
-  //       }
-
-  //   }
-    
-  // }
