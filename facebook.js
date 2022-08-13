9a9d391f41f5c5feffd04b885c8c0d82957abef2
dedepene <rvelitchkov@gmail.com>
2022-08-13 10:11:14 +0300

    now stories are removed too

--- facebook.js
+++ facebook.js
@@ -9,6 +9,7 @@ function getParentNode(element, level =1) {
 }
 
 function removeReels() {
+    //removeStories();
     reels = document.getElementsByClassName('d2edcug0 hpfvmrgz qv66sw1b c1et5uql lr9zc1uh a8c37x1j fe6kdd0r mau55g9w c8b282yb keod5gw0 nxhoafnm aigsh9s9 d3f4x2em mdeji52x a5q79mjw g1cxx5fr lrazzd5p oo9gr5id');
     for (let i=0; i <reels.length; i++){
         if (reels[i].innerHTML.indexOf('Reels and short') != -1) {
@@ -30,5 +31,14 @@ function removeReels() {
     }
 }
 
+function removeStories(){
+    const stories = document.getElementsByClassName('j83agx80 btwxx1t3 taijpn5t sjgh65i0 cxgpxx05');
+    while (stories.length > 0) {
+        stories[0].parentNode.removeChild(stories[0]);
+        console.log('Stories just got killed');
+    }
+}
+
 document.addEventListener ('scroll', removeReels);
+removeStories();
 
