7faceac1e6da9505c719fa2c60054d68f6f8bee3
dedepene <rvelitchkov@gmail.com>
2022-08-19 11:31:15 +0300

    added productivity switch to options

--- options.js
+++ options.js
@@ -1,6 +1,8 @@
 //select the two checkbox elements
 const reelsCheckBox = document.getElementById ('reels');
 const storiesCheckBox = document.getElementById ('stories');
+const productivityCheckBox = document.getElementById ('productivity');
+
 
 //when the page loads, also load the states of each toggle
 document.addEventListener('DOMContentLoaded', restore_options);
@@ -8,19 +10,22 @@ document.addEventListener('DOMContentLoaded', restore_options);
 //when each toggle is switched, update the toggle state in browser storage
 reelsCheckBox.addEventListener('change', save_options);
 storiesCheckBox.addEventListener('change', save_options);
+productivityCheckBox.addEventListener('change', save_options);
 
 
 
-console.log("reels box? ", reelsCheckBox.checked, "stories box?", storiesCheckBox.checked);
+console.log("reels box? ", reelsCheckBox.checked, "stories box?", storiesCheckBox.checked, "productivity box?", productivityCheckBox.checked);
 
 //update the status of toggles in browser storage
 function save_options() {
     let reels = document.getElementById('reels').checked;
     let stories = document.getElementById('stories').checked;
-    console.log('reels', reels, 'stories', stories);
+    let productivity = document.getElementById('productivity').checked;
+    console.log('reels', reels, 'stories', stories, 'productivity', productivity);
     chrome.storage.sync.set({
       checkedReels: reels,
-      chekedStories: stories
+      checkedStories: stories,
+      checkedProductivity: productivity
     }, function() {
       // Update status to let user know options were saved.
       let status = document.getElementById('status');
@@ -35,9 +40,11 @@ function save_options() {
   function restore_options() {
     chrome.storage.sync.get({
       checkedReels: true,
-      chekedStories: true
+      checkedStories: true,
+      checkedProductivity: true
     }, function(items) {
       document.getElementById('reels').checked = items.checkedReels;
-      document.getElementById('stories').checked = items.chekedStories;
+      document.getElementById('stories').checked = items.checkedStories;
+      document.getElementById('productivity').checked = items.checkedProductivity;
     });
 }
